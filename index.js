f6737fb70935fd4283c2e2c49ec2007b55ea1f84
Timothy <mwangitimothy789@gmail.com>
2023-03-10 10:59:11 +0300

    main

---
+++ index.js
@@ -0,0 +1,48 @@
+// create variables
+let name="javascript"
+console.log(name)
+
+let first_name="my"
+let second_name="name"
+let full_name= first_name + second_name
+console.log(full_name)
+
+
+let num=20
+if (num<10){
+    //code if condition is true
+    console.log("number is less than 10")
+} else {
+    /tcode of condition is false
+    console.log("number is greater than 10")
+}
+
+if else 
+    if (condition 1) {
+        //de of true
+    } else if (condition_2) {
+        // code of true
+    } else if (code_3) {
+        // code of true
+    } else {
+        // code of condition are false
+    }
+    num:1=15
+    if(num_1>10) {
+        console.log("greater than 20")
+    } else if (num_1==10){
+        console.log("less than 10")
+    }
+
+
+    let country="chad"
+    let age =18
+    if(country=="togo") {
+        !! code is true
+        if(age >=18) {
+            console.log("accepted")
+        }
+        } else{
+            console.log("not accepted")
+        }
+    
